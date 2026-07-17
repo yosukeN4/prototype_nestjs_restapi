ba139022db9a288360a33cd89b8945280a72ede9
yosukeN <yousken72@gmail.com>
2026-07-17 17:20:38 +0900

    [Sec.5-2] rewrite list 5-7 on app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
 
+
+const data = [
+  {name:'taro', mail:'taro@yamada', age:39},
+  {name:'hanako', mail:'hanako@flower', age:28},
+  {name:'sachiko', mail:'sachiko@happy', age:17}
+]
+
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  
+  getHello(id:number): object {
+    const n = id < 0 ? 0 : id >= data.length ? data.length - 1 : id;
+    return {
+      id:n,
+      data:data[n],
+      created:new Date()
+    };
   }
 }
